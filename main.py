e14322b5724a8165c9583dc66643b1d6e33b49e2
stvalentinewasblonde <valliemkvsk@mail.ru>
2025-12-21 11:35:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- import pygame
+import pygame
 
 
 class Sprite:
@@ -25,10 +25,42 @@ class Player(Sprite):
                 self.rect.y -= self.speed
             else:
                 self.rect.y += self.speed
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > 600:
+            self.rect.bottom = 600
 
 
+class Ball(Sprite):
+    def __init__(self, center, image, speed):
+        super().__init__(center, image)
+        self.speed = speed
+        self.velocity = pygame.Vector2(1,0)
 
+    def check_x_collision(self, player):
+        if self.rect.colliderect(player.rect):
+            if self.velocity.x > 0:
+                self.rect.right = player.rect.left
+            else:
+                self.rect.left = player.rect.right 
+            self.velocity.x = -self.velocity.x
 
+    def check_y_collision(self, player):
+        if self.rect.colliderect(player.rect):
+            if self.velocity.y > 0:
+                self.rect.bottom = player.rect.top
+            else:
+                self.rect.top = player.rect.bottom
+            self.velocity.y = -self.velocity.y
+
+    def update(self, left_player, right_player):
+        vector = self.velocity*self.speed
+        self.rect.x += vector.x
+        self.check_x_collision(left_player)
+        self.check_x_collision(right_player)
+        self.rect.y += vector.y
+        self.check_y_collision(left_player)
+        self.check_y_collision(right_player)
 window = pygame.Window('Ping Pong', (800, 600), pygame.WINDOWPOS_CENTERED)
 
 surface = window.get_surface()
@@ -37,8 +69,13 @@ clock = pygame.Clock()
 image = pygame.Surface((40, 100))
 image.fill('blue')
 left_player = Player((40, 300), image, 10)
-right_player = Player((40, 300), image, 10)
+right_player = Player((800-40, 300), image, 10)
 running = True
+
+image = pygame.Surface((30,30))
+image.fill('white')
+pygame.draw.aacircle(image, 'yellow', (15,15), 15)
+ball = Ball((400,300), image, 5)
 while running:
     # Обработка событий.
     for event in pygame.event.get():
@@ -66,14 +103,19 @@ while running:
                 right_player.move_down = False
 
     # Обновление объектов.
-left_player.update()
+    left_player.update()
+    right_player.update()
+    ball.update(left_player, right_player)
+
+   # if ball.rect.colliderect(left_player.rect) or ball.rect.colliderect(right_player.rect):
+      #  ball.velocity.x = -ball.velocity.x
     # Отрисовка.
     # RGB - (0-255, 0-255, 0-255, 0-255) - белый
     # (0-00, 0-00, 0-00) - черный
-surface.fill('white')
-left_player.render(surface)
-right_player.render(surface)
-left_player.rect.x += 1
-window.flip()
-clock.tick(60)
-window.title = str(round(clock.get_fps())) + " FPS" 
+    surface.fill('white')
+    left_player.render(surface)
+    right_player.render(surface)
+    ball.render(surface)
+    window.flip()
+    clock.tick(60)
+    window.title = str(round(clock.get_fps())) + " FPS" 
